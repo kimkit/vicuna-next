f20bdc1acc1150c823ec40f4685d1acb8d0211a5
wuwenbin <wenbin.wu@foxmail.com>
2017-12-10 07:44:34 +0800

    update example config

--- example/config/server.php
+++ example/config/server.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'daemonize' => false,
+    'worker_num' => 8,
+    'task_worker_num' => 0,
     'on_boot' => function ($server) {
         require dirname(APP_PATH).'/src/HttpHandler.php';
     },
